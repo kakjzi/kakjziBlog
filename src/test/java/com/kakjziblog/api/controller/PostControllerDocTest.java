ae08d07dd9fbb97b9484a8b562aa1c4126d67e68
kakjzi <anmern3@naver.com>
2022-09-03 22:42:54 +0900

    spring rest docs

---
+++ src/test/java/com/kakjziblog/api/controller/PostControllerDocTest.java
@@ -0,0 +1,92 @@
+package com.kakjziblog.api.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kakjziblog.api.domain.Post;
+import com.kakjziblog.api.repository.PostRepository;
+import com.kakjziblog.api.request.PostCreate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.restdocs.request.RequestDocumentation;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.kakjzi.com", uriPort = 443)
+@ExtendWith(RestDocumentationExtension.class)
+public class PostControllerDocTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    public PostRepository postRepository;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("글 단건 조회 테스트")
+    void test1() throws Exception {
+        Post post = Post.builder()
+                .title("123456789012345")
+                .content("bar")
+                .build();
+        postRepository.save(post);
+
+        mockMvc.perform(get("/posts/{postId}", 1L)
+                        .accept(APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("post-inquiry",
+                        pathParameters(
+                                RequestDocumentation.parameterWithName("postId")
+                                        .description("게시글 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("게시글ID"),
+                                fieldWithPath("title").description("제목"),
+                                fieldWithPath("content").description("내용")
+                        )
+                ));
+    }
+    @Test
+    @DisplayName("글 등록")
+    void test2() throws Exception {
+
+        PostCreate request = PostCreate.builder()
+                .title("나는 신지우")
+                .content("반포자이")
+                .build();
+
+        String json = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(post("/posts")
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("post-create",
+                        requestFields(
+                                PayloadDocumentation.fieldWithPath("title")
+                                        .description("제목").attributes(key("constraint").value("좋은 제목 입력해주세요")),
+                                PayloadDocumentation.fieldWithPath("content")
+                                        .description("내용").optional()
+                        )
+                ));
+
+    }
+}
