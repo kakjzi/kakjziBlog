3bb9f8b5584b493269596faf26c786808140ee69
kakjzi <anmern3@naver.com>
2023-05-08 21:31:46 +0900

    feat: Set up and test form login security

--- src/main/java/com/kakjziblog/api/config/SecurityConfig.java
+++ src/main/java/com/kakjziblog/api/config/SecurityConfig.java
@@ -8,7 +8,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 /**
  * Spring 6 부터는 Bean 주입 방식으로 변경됨
@@ -31,7 +38,31 @@ public class SecurityConfig {
 				   .requestMatchers("/auth/login").permitAll()
 				   .anyRequest().authenticated()
 			.and()
+			.formLogin()
+				.loginPage("/auth/login")
+				.loginProcessingUrl("/auth/login")
+				.usernameParameter("username")
+				.passwordParameter("password")
+				.defaultSuccessUrl("/")
+			.and()
+			.userDetailsService(userDetailsService())
 			.csrf(AbstractHttpConfigurer::disable)
 			.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		UserDetails user = User.withUsername("jiwoo")
+							   .password("1234")
+							   .roles("ADMIN")
+							   .build();
+		manager.createUser(user);
+		return manager;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 }
